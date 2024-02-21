84d987dfb1c92694166d17d8d399a2fb27ea9a89
shifashua <160706330+shifashua@users.noreply.github.com>
2024-02-21 21:29:01 +0700

    Update nav.js

--- nav.js
+++ nav.js
@@ -1,1 +1,8 @@
-
+function myFunction() {
+    var x = document.getElementById("res-nav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
